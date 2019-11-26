94f09326ae1e8b45bdb960aa5f0dd884ad0cd6c5
Bing Jian <bing.jian@jd.com>
2019-11-25 22:11:23 -0800

    Remove Python directory since there is already a separate repo http://github.com/bing-jian/gmmreg-python

--- Python/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-
-from _demo import test, run_executable
-from _plotting import display2Dpointsets, displayABC
--- Python/c_extension/memory_layout_note.h
+++
@@ -1,9 +0,0 @@
-/*
- Note:  the assumed memory layout is point-wise contiguous, 
- i.e. the j-th coordinate of the i-th point is at location  
- (i*d + j) where d is the dimensionality. Thus, the MATLAB 
- mex-function expects an input matrix where each point is a 
- d-dimensional column vector as MATLAB is column-major while 
- the input array to the NumPy should place each point as a 
- d-dimensional row vector since NumPy is row-major.
-*/
